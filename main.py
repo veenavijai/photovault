84b30436c3b28e9a26b4078b08a6bd927fad140d
Veena Vijai <veena.vijai42@gmail.com>
2025-03-16 00:10:36 -0700

    Add rough outline for file upload and download logic

--- main.py
+++ main.py
@@ -34,3 +34,37 @@ async def generate_session_token(code: int, device_id: str):
     hasher.update(str(code) + device_id)
     sessionToken = hasher.hexdigest()
     return {"session_token": sessionToken}
+
+@app.post("/file/:file_name")
+async def upload_file(sessionToken: str, file_name: str):
+    # Check if token is present
+
+    # Is the token well-formed
+
+    # Is the token expired?
+
+    # Verify token is associated with this user 
+
+    # Validate file type and size
+
+    # Store the file on the cloud/disk
+
+    # Have a DB associating user to the file
+
+@app.get("/file/:file_name")
+async def download_file(sessionToken: str, file_name: str):
+     # Check if token is present
+
+    # Is the token well-formed
+
+    # Is the token expired?
+
+    # Verify token is associated with this user 
+    
+    # Validate that file_name is associated with user
+    # Might be better to associate each file with a key here, 
+    # Because different users can use the same file_name
+    
+    # Retrieve file_name from DB
+    
+    # Return file byte stream and status code      
